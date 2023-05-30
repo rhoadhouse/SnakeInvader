5a899312c2cdf82ef6d20f34dd1c7f1c90a3d00b
Tyler Rhoades <tylerrhoades13@gmail.com>
2023-05-30 00:16:19 -0500

    fixed snake movement so that the body segments properly follwo the head and remain the proper distance from one another.

--- Snake.py
+++ Snake.py
@@ -11,7 +11,7 @@ class Snake():
         self.body = pygame.image.load("green_sphere.png")
         self.length = 10
         self.body_seg_positions =[(self.position_x - x*self.body.get_width(),self.position_y) for x in range(0,self.length)]
-        self.move_speed = self.body.get_width()/2
+        self.move_speed = self.body.get_width()/3
         self.vect_list = [(0,0) for x in range(0, self.length)]
 
 
@@ -33,22 +33,19 @@ class Snake():
 
     def move_right(self):
         self.set_velocity((1,0))
-        self.set_body_pos()
+        self.set_body_pos((1,0))
 
     def move_left(self):
         self.set_velocity((-1,0))
-        self.set_body_pos()
+        self.set_body_pos((-1,0))
 
     def move_up(self):
         self.set_velocity((0,-1))
-        self.set_body_pos()
+        self.set_body_pos((0,-1))
 
     def move_down(self):
         self.set_velocity((0,1))
-        self.set_body_pos()
-        global value
-        value += 1
-        print(value)
+        self.set_body_pos((0,1))
 
     def add_body_segment(self):
         pass
@@ -57,21 +54,23 @@ class Snake():
         pass
 
 
-    def set_body_pos(self):
-        def add_vec_to_pos(self, index):
-            return (self.body_seg_positions[index][0] + self.vect_list[index][0],self.body_seg_positions[index][1] + self.vect_list[index][1])
-        list = [add_vec_to_pos(self, index) for index in range(0, self.length)]
+    def set_body_pos(self, direction):
+        # def add_vec_to_pos(self, index):
+        #     return (self.body_seg_positions[index][0] + self.vect_list[index][0],self.body_seg_positions[index][1] + self.vect_list[index][1])
+        # list = [add_vec_to_pos(self, index) for index in range(0, self.length)]
+        # setattr(self, "body_seg_positions", list)
+        # print(self.vect_list)
+        def add_vec_to_pos(self, direction):
+            return (self.body_seg_positions[0][0] + direction[0]*(self.move_speed+int(self.body.get_width()/2)),self.body_seg_positions[0][1] + direction[1]*(self.move_speed+int(self.body.get_width()/2)))
+
+        list = self.body_seg_positions
+        list.pop(-1)
+        list.insert(0,add_vec_to_pos(self, direction))
+
+
         setattr(self, "body_seg_positions", list)
 
     def set_velocity(self, direction):
         """expects a tuple as an input representing the vector direction that the body is taking"""
         self.vect_list.pop(-1)
         self.vect_list.insert(0,(direction[0]*self.move_speed, direction[1]*self.move_speed))
-
-
-snake = Snake(0, [0,0])
-print(snake.vect_list)
-snake.set_velocity((1,-1))
-snake.set_body_pos()
-print(snake.vect_list)
-print(snake.body_seg_positions)
